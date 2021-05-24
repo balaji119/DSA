b511b2ab71745ccada6191a5bed82f959bc10b61
Balaji Kanagaraju <balajik@live.com>
2021-05-24 23:53:04 +0530

    Added two sum algorithm

---
+++ DSA.Test/TwoSumTest.cs
@@ -0,0 +1,17 @@
+﻿using DSA.Algorithms.Array;
+using NUnit.Framework;
+
+namespace DSA.Test
+{
+    public class TwoSumTest
+    {
+        [Test]
+        public void Test_TwoSum()
+        {
+            var array = new int[] { 3, 3 };
+            var result = TwoSumAlgorithm.TwoSum(array, 6);
+            Assert.AreEqual(result, new int[] { 0,1 });
+        }
+
+    }
+}
---
+++ DSA/Algorithms/Array/TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DSA.Algorithms.Array
+{
+    /**
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Example 1:
+
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+     **/
+    public class TwoSumAlgorithm
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                var diff = target - num;
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dict[diff], i };
+                }
+                dict[num] = i;
+            }
+
+            return null;
+        }
+    }
+}
